e4d310d349539419b59f5daa7dd8b277f19a020c
Ghazanfarijaz <i200870@nu.edu.pk>
2025-02-04 01:21:06 +0500

    search card

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -9,7 +9,11 @@ import { useGetCardByCardNumberQuery, useGetAllCardsQuery } from "../api/apiSlic
 import { Html5Qrcode } from "html5-qrcode";
 import GradientAreaChart from "./GradientGraph";
 import triangle from "../assets/triangle.png";
-import Hero from "./Hero";
+
+
+
+import polygon from "../assets/Polygon.png";
+import group from "../assets/Group.png";
 
 export default function SearchCard() {
   const swiperRef = useRef(null);
@@ -168,9 +172,164 @@ const handleScanSuccess = (decodedText) => {
   };
   return (
     <div className="min-h-[100vh] bg-transparent text-white flex flex-col items-center pt-28">
-       <Hero />
-      {/* Search Section */}
-      <div className="w-full max-w-8xl mb-8 px-4">
+       <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "110vh",
+        width: "100%",
+        position: "relative",
+        overflow: "hidden",
+        backgroundColor: "#1A1A1A", // Dark Page background
+      }}
+    >
+      {/* Background Gradient */}
+      <Box
+        sx={{
+          position: "absolute",
+          top: "40%",
+          left: "5%",
+          transform: "translateX(-50%)",
+          width: { md: "300px", xs: "150px" },
+          height: "150px",
+          background:
+            "radial-gradient(circle, rgba(2, 204, 254, 0.5) 0%, rgba(2, 204, 254, 0.65) 100%)",
+          filter: "blur(100px)",
+          zIndex: 0, // Place the gradient behind the content
+        }}
+      ></Box>
+      <div class="absolute top-[40%] left-[2%] h-12">
+        <img src={polygon} />
+      </div>
+      <div class="absolute top-[80%] sm:top-[10%] md:top-[75%] left-[60%] sm:left-[10%] md:left-[80%] h-12">
+        <img src={polygon} alt="polygon" />
+      </div>
+      <div class="absolute top-[75%]  left-[0%]  h-12">
+        <img src={group} alt="polygon" />
+      </div>
+      <Box
+        sx={{
+          position: "absolute",
+          top: { xs: "20%", md: "30%" },
+          left: { md: "90%", xs: "55%" },
+          transform: "translateX(-50%)",
+          width: { md: "300px", xs: "150px" },
+          height: "150px",
+          background:
+            "radial-gradient(circle, rgba(2, 204, 254, 0.5) 0%, rgba(2, 204, 254, 0.65) 100%)",
+          filter: "blur(100px)",
+          zIndex: 0, // Place the gradient behind the content
+        }}
+      />
+      <Box
+        sx={{
+          position: "absolute",
+          top: "95%",
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "1589px",
+          height: "194px",
+          background:
+            "radial-gradient(circle, rgba(2, 204, 254, 0.5) 0%, rgba(2, 204, 254, 0.2) 20%)",
+          filter: "blur(100px)",
+          zIndex: 0, // Place the gradient behind the content
+        }}
+      />
+      {/* Outer Box with Gradient Border */}
+      <Box
+        sx={{
+          position: "relative",
+          width: { xs: "100%", md: "500px" },
+          borderRadius: "35px",
+          background: "linear-gradient(to bottom, #121212, #02CCFE)", // Gradient border
+          padding: "4px", // Border thickness
+          zIndex: 1, // Content on top
+        }}
+      >
+        {/* Inner Box */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "12px 24px",
+            borderRadius: "31px", // Slightly smaller than the outer border
+            backgroundColor: "#121212", // Solid black background for content
+          }}
+        >
+          {/* Main Text */}
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              fontSize: { md: "16px", xs: "12px" },
+              color: "#FFFFFF",
+            }}
+          >
+            Unleash the Full Potential of Your Cards
+          </Typography>
+
+          {/* Discover More */}
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              fontSize: { md: "16px", xs: "12px" },
+              color: "#F27013", // Orange color
+              cursor: "pointer",
+            }}
+          >
+            Discover More
+          </Typography>
+        </Box>
+      </Box>
+
+      {/* Title */}
+      <Typography
+        sx={{
+          color: "white",
+          fontSize: { md: "80px", xs: "32px" },
+          fontWeight: "bold",
+          textAlign: "center",
+          mt: "4vh",
+          letterSpacing: "3px", // Add letter spacing
+          zIndex: 1,
+        }}
+      >
+        Welcome to Sky Grading— <br />
+        Your Trusted Card Grading <br /> Service!
+      </Typography>
+
+      {/* Subtitle */}
+      <Typography
+        sx={{
+          color: "white",
+          fontSize: "20px",
+          textAlign: "center",
+          mt: "2vh",
+          zIndex: 1,
+        }}
+      >
+        Experience unparalleled professionalism and ease in card grading,
+        tailored
+        <br /> just for you.
+      </Typography>
+
+
+      <Box sx={{ width: "60%", padding: "10px 0" }}>
+      <Divider
+        sx={{
+          borderColor: "white", // White line
+          borderWidth: "2px", // Thicker line
+          opacity: 0.2, // Slight transparency for a subtle look
+          marginTop: 2,
+          marginBottom: 2
+        }}
+      />
+    </Box>
+
+           {/* Search Section */}
+           <div className="w-full max-w-8xl mb-8 px-4">
       <div className="w-full max-w-8xl mb-8 px-4">
   <div className="flex flex-wrap gap-4 items-center justify-center">
     {/* TextField */}
@@ -231,6 +390,10 @@ const handleScanSuccess = (decodedText) => {
   ></div>
 )} */}
       </div>
+    </Box>
+
+
+ 
 
       {/* QR Scanner */}
       {isScanning && (
